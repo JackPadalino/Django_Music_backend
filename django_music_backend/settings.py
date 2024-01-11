95b34f8a2a3a91654187802c75e2bb14b4e40997
JackPadalino <pada0867@gmail.com>
2024-01-11 17:48:08 -0500

    updatd environment variables and allowed origin

--- django_music_backend/settings.py
+++ django_music_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='backend_secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='BACKEND_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = []
 
 ROOT_URLCONF = 'django_music_backend.urls'
 
