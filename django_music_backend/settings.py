7a9afdba91f5101071eca3d4a441532c3f22e493
JackPadalino <pada0867@gmail.com>
2024-01-11 17:49:39 -0500

    updated environment variables and allowed origins

--- django_music_backend/settings.py
+++ django_music_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='BACKEND_SECRET_KEY')
+SECRET_KEY = os.environ.get('BACKEND_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
