98607e50aa5596890165527aeef0866b5bb1c98c
JackPadalino <pada0867@gmail.com>
2024-01-11 19:06:33 -0500

    added allowed origin for local host to test front end

--- django_music_backend/settings.py
+++ django_music_backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 ROOT_URLCONF = 'django_music_backend.urls'
 
